11662f2382373c8f6768815c36c548bc57249bc9
Sergjio97 <sergiocizmja@gmail.com>
2021-11-07 12:52:24 +0100

    fine esercizio + bonus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // alert ("Hello world")
+
+let km = prompt("Quanti km devi percorrere?");
+
+let anni = prompt("Inserisci la tua età");
+
+let prezzo = 0.21 * km; 
+
+if (anni < 18) {
+    prezzo = 0.21 * km - 0.21 * km * 20 / 100
+} else if (anni > 65) {
+    prezzo = 0.21 * km - 0.21 * km * 40 / 100
+}
+
+document.getElementById("costo").innerHTML = "€" + " " + prezzo.toFixed(2);
